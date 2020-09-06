388f9d0d2df27583d67a2de6286d0506d9b9663a
Dustin Davis <dustin.davis@verizonmedia.com>
2020-09-06 10:25:00 -0600

    oops

--- src/html.js
+++ src/html.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const trackingCode = `
-<script type="text/javascript">
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
@@ -13,8 +12,7 @@ const trackingCode = `
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>`
+  })();`
 
 export default class HTML extends React.Component {
   render() {
