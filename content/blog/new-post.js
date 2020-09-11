ad97b9adb9fba07fc4723d2200d2a94c9d395ce5
Dustin Davis <dustin.davis@verizonmedia.com>
2020-09-11 11:57:52 -0600

    script updates

--- content/blog/new-post.js
+++ content/blog/new-post.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 
 const fs = require('fs')
+const opn = require('opn')
 const path = require('path')
 const readline = require('readline-sync')
 const slugify = require('@sindresorhus/slugify')
@@ -23,19 +24,22 @@ const categories = []
 strCat.split(',').forEach(category => {
   categories.push(category.trim())
 })
+opn(`https://unsplash.com/search/photos/${encodeURIComponent(title)}`, {
+  wait: false,
+})
 const bannerUrl = readline.question(
   'If you would like an Unsplash banner, enter the URL here:\n',
 )
 console.log(bannerUrl)
 ;(async () => {
   const today = new Date().toISOString().split('T')[0]
-  const slug = slugify(`${today} ${title}`)
+  const folderName = slugify(`${today} ${title}`)
 
-  fs.mkdirSync(path.join('.', slug, 'images'), {recursive: true})
-  console.log(`${slug} created`)
+  fs.mkdirSync(path.join('.', folderName, 'images'), {recursive: true})
+  console.log(`${folderName} created`)
 
   let bannerCredit = 'Photo by []() on [Unsplash](https://unsplash.com)'
-  const bannerPath = path.join(slug, 'images', 'banner.jpg')
+  const bannerPath = path.join(folderName, 'images', 'banner.jpg')
   if (bannerUrl) {
     banner.downloadImage(bannerUrl, bannerPath)
     bannerCredit = await banner.getCredits(bannerUrl)
@@ -44,9 +48,9 @@ console.log(bannerUrl)
     fs.copyFileSync('./banner.jpg', bannerPath)
   }
 
-  const indexPath = path.join('.', slug, 'index.md')
+  const indexPath = path.join('.', folderName, 'index.md')
   const frontMatter = {
-    slug,
+    slug: slugify(title),
     title,
     date: today,
     author: 'Dustin Davis',
